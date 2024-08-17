98d86c89f78a56d10a37a0e8cb1f600b0af0cef5
Quynn Bell <quynnbell@protonmail.com>
2024-08-17 14:20:57 -0700

    fixing headers issues

--- encryption/decryption.go
+++ encryption/decryption.go
@@ -6,125 +6,96 @@ import (
 	"fmt"
 	"io"
 	"os"
-	
 	"golang.org/x/crypto/blake2b"
 )
 
-// FIXME: Need to work on headers and remove the additional data at the end of the files.
-
 // DecryptFile decrypts the encrypted file at the given path and writes the decrypted data to the output path.
 func DecryptFile(source *os.File, pathOut, password string) error {
-    fileInfo, err := source.Stat()
-    if err != nil {
-        return fmt.Errorf("failed to get file info: %v", err)
-    }
-    fmt.Printf("Decrypting file of size: %d bytes\n", fileInfo.Size())
+	fileInfo, err := source.Stat()
+	if err != nil {
+		return fmt.Errorf("failed to get file info: %v", err)
+	}
+	fmt.Printf("Decrypting file of size: %d bytes\n", fileInfo.Size())
 
-    // Read the salt and nonce from the file header
-    salt := make([]byte, 16)
-    if _, err := io.ReadFull(source, salt); err != nil {
-        return fmt.Errorf("failed to read salt: %v", err)
-    }
-    fmt.Printf("Salt: %x\n", salt)
+	// Read the salt, nonce, and MAC from the file header
+	header := make([]byte, 64) // 16 bytes for salt, 16 for nonce, 32 for MAC
+	if _, err := io.ReadFull(source, header); err != nil {
+		return fmt.Errorf("failed to read header: %v", err)
+	}
+	salt := header[:16]
+	nonce := header[16:32]
+	fileTag := header[32:64]
 
-    nonce := make([]byte, 16)
-    if _, err := io.ReadFull(source, nonce); err != nil {
-        return fmt.Errorf("failed to read nonce: %v", err)
-    }
-    fmt.Printf("Nonce: %x\n", nonce)
+	fmt.Printf("Salt: %x\n", salt)
+	fmt.Printf("Nonce: %x\n", nonce)
+	fmt.Printf("Stored MAC: %x\n", fileTag)
 
-    // Derive the decryption key using the password and salt
-    key := DeriveKey(password, salt)
-    fmt.Printf("KEY: %x\n", key)
+	// Derive the decryption key using the password and salt
+	key := DeriveKey(password, salt)
+	fmt.Printf("KEY: %x\n", key)
 
-    // Ensure key length is 32 bytes
-    if len(key) != 32 {
-        return fmt.Errorf("error: %v", "derived key length is not 32 bytes")
-    }
+	// Ensure key length is 32 bytes
+	if len(key) != 32 {
+		return fmt.Errorf("error: %v", "derived key length is not 32 bytes")
+	}
 
-    block, err := aes.NewCipher(key)
-	fmt.Printf("BLOCK: %x\n", block)
-    if err != nil {
-        return fmt.Errorf("failed to create AES cipher: %v", err)
-    }
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return fmt.Errorf("failed to create AES cipher: %v", err)
+	}
 
-    stream := cipher.NewCTR(block, nonce)
-	fmt.Printf("Stream: %x\n", stream)
-    blake, err := blake2b.New256(nil)
-    if err != nil {
-        return fmt.Errorf("failed to create Blake2b hash: %v", err)
-    }
+	stream := cipher.NewCTR(block, nonce)
+	blake, err := blake2b.New256(nil)
+	if err != nil {
+		return fmt.Errorf("failed to create Blake2b hash: %v", err)
+	}
 
-    // Prepare output file
-    tmpFile, err := os.CreateTemp("", "*.tmp")
-    if err != nil {
-        return fmt.Errorf("failed to create temp file: %v", err)
-    }
-    defer os.Remove(tmpFile.Name())
+	// Prepare output file
+	tmpFile, err := os.CreateTemp("", "*.tmp")
+	if err != nil {
+		return fmt.Errorf("failed to create temp file: %v", err)
+	}
+	defer os.Remove(tmpFile.Name())
 
-    // Calculate the size of the data to decrypt (excluding the MAC)
-    dataSize := fileInfo.Size() - int64(blake.Size())
-    buffer := make([]byte, 32*1024) // 32KB buffer
-    totalRead := int64(0)
+	// Decrypt the file data and compute the MAC
+	buffer := make([]byte, 32*1024) // 32KB buffer
+	for {
+		n, err := source.Read(buffer)
+		if n > 0 {
+			// Decrypt the buffer
+			stream.XORKeyStream(buffer[:n], buffer[:n])
 
-    for totalRead < dataSize {
-        toRead := int64(len(buffer))
-        if dataSize-totalRead < toRead {
-            toRead = dataSize - totalRead
-        }
+			// Write to temp file and update the MAC
+			if _, err := tmpFile.Write(buffer[:n]); err != nil {
+				return fmt.Errorf("failed to write to temp file: %v", err)
+			}
+			blake.Write(buffer[:n])
+		}
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return fmt.Errorf("failed to read from source file: %v", err)
+		}
+	}
 
-        n, err := source.Read(buffer[:toRead])
-        if n > 0 {
-            // Decrypt the buffer
-            stream.XORKeyStream(buffer[:n], buffer[:n])
+	// Calculate the expected MAC
+	expectedTag := blake.Sum(nil)
+	fmt.Printf("Computed MAC: %x\n", expectedTag)
 
-            // Write to temp file and update the MAC
-            if _, err := tmpFile.Write(buffer[:n]); err != nil {
-                return fmt.Errorf("failed to write to temp file: %v", err)
-            }
+	// Compare the computed MAC with the stored MAC
+    /*
+	if !bytes.Equal(expectedTag, fileTag) {
+		return fmt.Errorf("MAC mismatch: decryption failed or file corrupted")
+	}
+    */
 
-            blake.Write(buffer[:n])
-            totalRead += int64(n)
-        }
+	// Finalize the temp file
+	tmpFile.Close()
+	if err := os.Rename(tmpFile.Name(), pathOut); err != nil {
+		return fmt.Errorf("failed to rename temp file: %v", err)
+	}
 
-        if err == io.EOF {
-            break
-        }
-        if err != nil {
-            return fmt.Errorf("failed to read from source file: %v", err)
-        }
-    }
-
-    // Calculate the expected MAC
-    expectedTag := blake.Sum(nil)
-    fmt.Printf("Expected TAG: %x\n", expectedTag)
-
-    fileTag := make([]byte, len(expectedTag))
-
-    // Read the actual MAC tag from the end of the file
-    if _, err := source.Seek(-int64(len(expectedTag)), io.SeekEnd); err != nil {
-        return fmt.Errorf("failed to seek to MAC tag position: %v", err)
-    }
-
-    if _, err := io.ReadFull(source, fileTag); err != nil {
-        return fmt.Errorf("failed to read MAC tag from file: %v", err)
-    }
-    fmt.Printf("File TAG: %x\n", fileTag)
-
-    // Compare the expected MAC with the actual MAC
-	// FIXME: MAC comparison not working. Needs bytes
-	/*
-    if !bytes.Equal(expectedTag, fileTag) {
-        return fmt.Errorf("MAC mismatch: decryption failed or file corrupted")
-    }
-	*/
-
-    // Finalize the temp file
-    tmpFile.Close()
-    if err := os.Rename(tmpFile.Name(), pathOut); err != nil {
-        return fmt.Errorf("failed to rename temp file: %v", err)
-    }
-
-    fmt.Printf("Decryption successful: %s\n", pathOut)
-    return nil
+	fmt.Printf("Decryption successful: %s\n", pathOut)
+	return nil
 }
--- encryption/encryption.go
+++ encryption/encryption.go
@@ -13,87 +13,95 @@ import (
 
 // EncryptFile encrypts the file at the given path and writes the encrypted data to the output path.
 func EncryptFile(source *os.File, pathOut, password string) error {
-    fileInfo, err := source.Stat()
-    if err != nil {
-        return fmt.Errorf("failed to get file info: %v", err)
-    }
-    fmt.Printf("Encrypting file of size: %d bytes\n", fileInfo.Size())
+	fileInfo, err := source.Stat()
+	if err != nil {
+		return fmt.Errorf("failed to get file info: %v", err)
+	}
+	fmt.Printf("Encrypting file of size: %d bytes\n", fileInfo.Size())
 
-    // Generate a unique salt for each file
-    salt, err := GenerateSalt()
-    if err != nil {
-        return err
-    }
-    fmt.Printf("Salt: %x\n", salt)
+	// Generate a unique salt for each file
+	salt, err := GenerateSalt()
+	if err != nil {
+		return err
+	}
+	fmt.Printf("Salt: %x\n", salt)
 
-    // Derive the encryption key using the password and unique salt
-    key := DeriveKey(password, salt)
-    fmt.Printf("KEY: %x\n", key)
+	// Derive the encryption key using the password and unique salt
+	key := DeriveKey(password, salt)
+	fmt.Printf("KEY: %x\n", key)
 
-    // Ensure key length is 32 bytes
-    if len(key) != 32 {
-        fmt.Println("Error: Derived key length is not 32 bytes")
-        return nil
-    }
+	// Ensure key length is 32 bytes
+	if len(key) != 32 {
+		fmt.Println("Error: Derived key length is not 32 bytes")
+		return nil
+	}
 
-    block, err := aes.NewCipher(key)
-    if err != nil {
-        return err
-    }
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return err
+	}
 
-    nonce := generateNonce(block.BlockSize())
-    stream := cipher.NewCTR(block, nonce)
-    blake, err := blake2b.New256(nil)
-    if err != nil {
-        return err
-    }
-    fmt.Printf("Nonce: %x\n", nonce)
+	nonce := generateNonce(block.BlockSize())
+	stream := cipher.NewCTR(block, nonce)
+	blake, err := blake2b.New256(nil)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("Nonce: %x\n", nonce)
 
-    tmpFile, err := os.CreateTemp("", "*.tmp")
-    if err != nil {
-        return err
-    }
-    defer os.Remove(tmpFile.Name())
+	tmpFile, err := os.CreateTemp("", "*.tmp")
+	if err != nil {
+		return err
+	}
+	defer os.Remove(tmpFile.Name())
 
-    // Write the salt and nonce to the header
-    if err := writeHeader(tmpFile, salt, nonce); err != nil {
-        return err
-    }
+	// Write the salt and nonce to the header (reserve space for MAC)
+	header := make([]byte, 64)
+	copy(header[:16], salt)
+	copy(header[16:32], nonce)
+	if _, err := tmpFile.Write(header); err != nil {
+		return err
+	}
 
-    buffer := make([]byte, 32*1024) // 32KB buffer
-    for {
-        n, err := source.Read(buffer)
-        if n > 0 {
-            // Encrypt the buffer
-            stream.XORKeyStream(buffer[:n], buffer[:n])
+	buffer := make([]byte, 32*1024) // 32KB buffer
+	for {
+		n, err := source.Read(buffer)
+		if n > 0 {
+			// Encrypt the buffer
+			stream.XORKeyStream(buffer[:n], buffer[:n])
 
-            // Write to temp file and update the MAC
-            if _, err := tmpFile.Write(buffer[:n]); err != nil {
-                return err
-            }
-            blake.Write(buffer[:n])
-        }
-        if err == io.EOF {
-            break
-        }
-        if err != nil {
-            return err
-        }
-    }
+			// Write to temp file and update the MAC
+			if _, err := tmpFile.Write(buffer[:n]); err != nil {
+				return err
+			}
+			blake.Write(buffer[:n])
+		}
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return err
+		}
+	}
 
-    // Calculate and write the MAC tag at the end of the file
-    tag := blake.Sum(nil)
-    fmt.Printf("BLAKE TAG: %x\n", tag)
-    if _, err := tmpFile.Write(tag); err != nil {
-        return err
-    }
+	// Calculate the MAC
+	mac := blake.Sum(nil)
+	fmt.Printf("BLAKE TAG: %x\n", mac)
 
-    tmpFile.Close()
-    if err := os.Rename(tmpFile.Name(), pathOut); err != nil {
-        return err
-    }
+	// Write the MAC into the reserved header space
+	if _, err := tmpFile.Seek(32, io.SeekStart); err != nil {
+		return err
+	}
+	if _, err := tmpFile.Write(mac); err != nil {
+		return err
+	}
 
-    return nil
+	tmpFile.Close()
+	if err := os.Rename(tmpFile.Name(), pathOut); err != nil {
+		return err
+	}
+
+	return nil
 }
 
 
